18d8f011abd352c3c9263760e72bda6b907fc413
KonuTech <borowiec.k@gmail.com>
2023-01-24 16:56:51 +0100

    timezonedb-postgresql

--- app.py
+++ app.py
@@ -7,38 +7,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-ZONE_NAME_PROMPT = input(
-    """
-        Enter a Zone Name.
-        Press ENTER for default value: America/New_York:
-    """
-)
+ZONE_TIME = open("scripts/sql/dql/zone_time.sql", 'r').read()
 
 
-CURRENT_TIME = open("scripts/sql/dql/current_time.sql", 'r').read()
-
-
-def current_time(zone_name='America/New_York'):
-
-    conn = psycopg2.connect(os.environ["DATABASE_URI"])
-    try:
-        with conn:
-            with conn.cursor() as cur:
-                cur.execute(CURRENT_TIME, (zone_name,))
-                print(f"Current Time for selected Zone name: {cur.fetchone()}")
-
-                # commit the transaction
-                conn.commit()
-
-                # close the cursor
-                cur.close()
-
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+def menu():
+    zone_name = input("Enter a Zone Name. Press ENTER for default value: America/New_York: ") or 'America/New_York'
+    if zone_name is not None:
+        conn = psycopg2.connect(os.environ["DATABASE_URI"])
+        try:
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute(ZONE_TIME, (zone_name,))
+                    print(f"Current Time for selected Zone name: {cur.fetchone()}")
+                    cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
 
 
 if __name__ == '__main__':
-    current_time()
+    menu()
